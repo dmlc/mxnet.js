45d04ec435f9214eeec02b87fd9775831658372a
tqchen <tianqi.tchen@gmail.com>
2015-12-03 21:43:42 -0800

    add timing

--- classify_image.js
+++ classify_image.js
@@ -29,8 +29,15 @@ function preproc(url, targetLen, meanimg, callback) {
       data[stride * 1 + i] = imgdata.data[i * 4 + 1];
       data[stride * 2 + i] = imgdata.data[i * 4 + 2];
     }
-    for (var i = 0; i < data.length; ++i) {
-      data[i] = data[i] - meanimg.data[i];
+    if (typeof meanimg !== 'undefined') {
+      for (var i = 0; i < data.length; ++i) {
+        data[i] = data[i] - meanimg.data[i];
+      }
+    } else {
+      // use 117 as mean by default.
+      for (var i = 0; i < data.length; ++i) {
+        data[i] = data[i] - 117;
+      }
     }
     var nd = ndarray(data, [1, 3, targetLen, targetLen]);
     callback(nd);
@@ -39,15 +46,15 @@ function preproc(url, targetLen, meanimg, callback) {
 }
 
 function start() {
-   $.getJSON("../model/inception-bn-model.json", function(model) {
+   $.getJSON("../model/fastpoor.json", function(model) {
        var url = document.getElementById("imageURL").value;
        pred = new Predictor(model, {'data': [1, 3, 224, 224]});
-
        preproc(url, 224, pred.meanimg,  function(nd) {
            pred.setinput('data', nd);
            logEvent("start... prediction... this can take a while");
            // delay 1sec before running prediction, so the log event renders on webpage.
            setTimeout(function(){
+               var start = new Date().getTime();
                pred.forward();
                logEvent("finished prediction...");
                out = pred.output(0);
@@ -55,7 +62,9 @@ function start() {
                for (var i = 0; i < out.data.length; ++i) {
                    if (out.data[max_index] < out.data[i]) max_index = i;
                }
-               logEvent('Top-1: ' + model.synset[max_index] + ', value=' + out.data[max_index]);
+               var end = new Date().getTime();
+               var time = (end - start) / 1000;
+               logEvent('Top-1: ' + model.synset[max_index] + ', value=' + out.data[max_index] + ', time-cost=' + time + 'secs');
                pred.destroy();
            }, 1000);
        });
