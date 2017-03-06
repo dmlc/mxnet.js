f8a3287c83551b99a99c8c1e03aa8e589dbf3213
Piyush Singh <mrsinghpiyush@gmail.com>
2017-03-06 20:52:42 +0000

    Add new files

---
+++ model/prepare_models.sh
@@ -0,0 +1,198 @@
+#!/usr/bin/env bash
+script_name=$0
+
+USAGE (){
+    echo "  "
+    echo "  "
+    echo "Script downloads model from the MXNet Model Gallery "
+    echo "  and prepares a combined JSON file containing the  "
+    echo "  computation graph and weights."
+    echo "Usage:"
+    echo "${script_name} [-squeezenet] [-nin] [-caffenet] [-resnet] [-inceptionbn]"
+    echo "  "
+    echo "  "
+}
+
+
+
+
+# model2json.py script does the same as this function
+prep_json_for_js(){
+  # To call function
+  #prep_json_for_js Inception-BN-symbol Inception-BN-0126.params
+
+  outFile=$1
+  inFile=$2
+  paramsFile=$3
+  synsetFile=$4
+
+  jsName=$outFile
+
+
+  # Create Symbol + params file for JSON
+  cp $inFile $jsName
+  sed -i '1s/^/{\n"symbol":\n/' $jsName
+  sed -i '$s/$/,/' $jsName
+  echo -en "\n" >> $jsName
+  cat $synsetFile | sed 's/.*/"&",/' | tr '\n' ' ' | sed 's/.*/"synset": [&],/' | sed 's/, ],/],/g' >> $jsName
+  echo -en "\n" >> $jsName
+  base64 -w 0 $paramsFile | sed 's/.*/"parambase64": "&"/' >> $jsName
+  echo -en "\n" >> $jsName
+  echo } >> $jsName
+}
+
+
+
+
+
+prep_resnet_model(){
+  echo "Preparing resnet18 model..."
+
+  echo "    Downloading model from gallery..."
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/synset.txt
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/resnet/18-layers/resnet-18-0000.params
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/resnet/18-layers/resnet-18-symbol.json
+
+  echo "   Running python script to generate json model for JS..."
+  prep_json_for_js ../resnet-model.json resnet-18-symbol.json resnet-18-0000.params synset.txt
+
+}
+
+prep_vgg_model(){
+  echo "Preparing vgg-19 model..."
+
+  echo "    Downloading model from gallery..."
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/synset.txt
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/vgg/vgg19-0000.params
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/vgg/vgg19-symbol.json
+
+  echo "   Running python script to generate json model for JS..."
+  prep_json_for_js ../vgg-model.json vgg19-symbol.json vgg19-0000.params synset.txt
+
+}
+
+prep_nin_model(){
+  echo "Preparing caffenet model..."
+
+  echo "    Downloading model from gallery..."
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/synset.txt
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/nin/nin-0000.params
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/nin/nin-symbol.json
+
+  echo "   Running python script to generate json model for JS..."
+  prep_json_for_js ../nin-model.json nin-symbol.json nin-0000.params synset.txt
+
+}
+
+prep_caffenet_model(){
+  echo "Preparing caffenet model..."
+
+  echo "    Downloading model from gallery..."
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/synset.txt
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/caffenet/caffenet-symbol.json
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/caffenet/caffenet-0000.params
+
+  echo "   Running python script to generate json model for JS..."
+  prep_json_for_js ../caffenet-model.json caffenet-symbol.json caffenet-0000.params synset.txt
+
+}
+
+prep_squeezenet_model(){
+  echo "Preparing squeezenet model..."
+
+  echo "    Downloading model from gallery..."
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/synset.txt
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/squeezenet/squeezenet_v1.0-symbol.json
+  wget --no-check-certificate http://data.dmlc.ml/models/imagenet/squeezenet/squeezenet_v1.0-0000.params
+
+  echo "   Running python script to generate json model for JS..."
+  prep_json_for_js ../squeezenet-model.json squeezenet_v1.0-symbol.json squeezenet_v1.0-0000.params synset.txt
+
+}
+
+prep_inception_model(){
+  echo "Preparing inceptionbn model..."
+
+  echo "    Downloading inception model from gallery..."
+  wget --no-check-certificate http://data.dmlc.ml/mxnet/models/imagenet/inception-bn.tar.gz
+  tar -zxvf inception-bn.tar.gz
+
+  echo "   Running python script to generate json model for JS..."
+  prep_json_for_js ../inception-bn-model.json Inception-BN-symbol.json Inception-BN-0126.params synset.txt
+
+}
+
+
+#
+# Parse command-line arguments
+#
+while [ "$1" != "" ]; do
+  case $1 in
+    -help)
+      USAGE
+      exit 0;;
+      -all | -inceptionbn | -squeezenet | -nin | -caffenet | -resnet | -vgg)
+      TYPE=${1:1};;
+  esac
+  shift
+done
+
+if [ ! "$TYPE" ]; then
+  echo "You must specify a test type"
+  USAGE
+  exit 0
+fi
+
+
+#
+# Create temp dir
+#
+THIS_DIR=$(cd `dirname $0`; pwd)
+TEMP_DIR="${THIS_DIR}/temp/"
+
+if [[ ! -d "${TEMP_DIR}" ]]; then
+  echo "${TEMP_DIR} doesn't exist, will create one";
+  mkdir -p ${TEMP_DIR}
+fi
+cd ${TEMP_DIR}
+
+#
+# Prepare models
+#
+case $TYPE in
+  all)
+    echo "Preparing all models..."
+    prep_nin_model
+    prep_inception_model
+    prep_squeezenet_model
+    prep_resnet_model
+    ;;
+  nin)
+    prep_nin_model
+    ;;
+  inceptionbn)
+    prep_inception_model
+    ;;
+  squeezenet)
+    prep_squeezenet_model
+    ;;
+  resnet)
+    prep_resnet_model
+    ;;
+  vgg)
+    prep_vgg_model
+    ;;
+  caffenet)
+    prep_caffenet_model
+    ;;
+esac
+
+echo "   Cleaning..."
+sleep 2
+cd ${THIS_DIR}
+rm -rf ${TEMP_DIR}
+
+echo "Done."
+echo " "
+echo "Contents for this dir:"
+ls -ltrh
--- test_on_node.js
+++ test_on_node.js
@@ -1,23 +1,69 @@
+// To run:
+//   docker run -v "$PWD":/usr/src/app -w /usr/src/app node:4 node test_on_node.js
+
+// External Includes
 var mx = require("./mxnet_predict.js");
-var model = require("./model/fastpoor.json");
+
+function runModel(modelJson, cat) {
+    console.log("  ");
+    console.log("  ");
+    console.log("Running model %s: ", modelJson);
+    console.log("  ");
+
+    var model = require(modelJson);
+    pred = new mx.Predictor(model, {'data': [1, 3, 224, 224]});
+    pred.setinput('data', cat);
+    var nleft = 1;
+
+    var start = new Date().getTime();
+    var end   = new Date().getTime();
+    var time  = (end - start) / 1000;
+
+    for (var step = 0; nleft != 0; ++step) {
+      nleft = pred.partialforward(step);
+      end = new Date().getTime();
+      time = (end - start) / 1000;
+      console.log("    progress " + (step+1) + "/" + (nleft+step+1) + "  Time=" + time + "s");
+    }
+    out = pred.output(0);
+
+    out = pred.output(0);
+    var index = new Array();
+    for (var i=0;i<out.data.length;i++) {
+        index[i] = i;
+    }
+    index.sort(function(a,b) {return out.data[b]-out.data[a];});
+
+    max_output = 10;
+    console.log("Finished. Top %d predictions: ", max_output);
+    for (var i = 0; i < max_output; i++) {
+        console.log("    [%d]: %s, PROB=%d%", (i+1), model.synset[index[i]], out.data[index[i]]*100);
+    }
+    pred.destroy();
+}
+
+
+//
+// Prepare input data
+//
 var cat_encoded = require("./data/cat.base64.json");
 var decode = mx.base64Decode(cat_encoded);
 var decoded = new Float32Array(decode.buffer);
 var cat = mx.ndarray(decoded, [1, 3, 224, 224]);
 
-pred = new mx.Predictor(model, {'data': [1, 3, 224, 224]});
-pred.setinput('data', cat);
-console.log("Here");
-var nleft = 1;
-for (var step = 0; nleft != 0; ++step) {
-  nleft = pred.partialforward(step);
-  console.log("progress " + (step+1) + "/" + (nleft+step+1));
-}
-out = pred.output(0);
-max_index = 0;
-for (var i = 0; i < out.data.length; ++i) {
-  if (out.data[max_index] < out.data[i]) max_index = i;
-}
-console.log('Top-1: ' + model.synset[max_index] + ', value=' + out.data[max_index]);
-pred.destroy();
 
+//
+// Models
+//
+// -- run "./models/prepare_models.sh -all" first to run the other two
+var modelJSONs = [ "./model/inception-bn-model.json",
+                   "./model/squeezenet-model.json",
+                   "./model/resnet-model.json",
+                   "./model/nin-model.json"]
+
+//
+// Run all models
+//
+for (var i=0; i<modelJSONs.length-2; i++) {
+    runModel(modelJSONs[i], cat);
+}
